c78e60050c692911b7961bb195e2237d44e8f0ea
ArtyomTregubov <SemperIdem1989@gmail.com>
2023-09-07 00:26:55 +0500

    fix7

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,8 @@ const URI_MONGO = 'mongodb://localhost:27017/mestodb';
 mongoose.connect(URI_MONGO);
 // mongoose.connect('mongodb://admin:admin@127.0.0.1:27017/mestodb?authSource=admin');
 
+const { NODE_ENV } = process.env;
+
 app.get('/crash-test', () => {
   setTimeout(() => {
     throw new Error('Сервер сейчас упадёт');
@@ -29,7 +31,11 @@ app.get('/crash-test', () => {
 app.use(limiter);
 app.use(requestLogger);
 app.use(policy);
-app.use('/api', routers);
+if (NODE_ENV === 'production') {
+  app.use('/api', routers);
+} else {
+  app.use(routers);
+}
 app.use(errorLogger);
 app.use(errors());
 app.use(handleError);
